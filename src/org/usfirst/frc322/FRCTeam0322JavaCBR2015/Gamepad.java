d5d98c272cc783f1529b8eeea4071ce4314bbf49
Raa'Shaun H. <CardcaptorRaaShaun@gmail.com>
2015-02-25 19:09:20 -0500

    Major Commit
    This, unfortunately, is what happens when I get 'in the zone' and forget
    to commit as I work.  I'll try to separate things later.

---
+++ src/org/usfirst/frc322/FRCTeam0322JavaCBR2015/Gamepad.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc322.FRCTeam0322JavaCBR2015;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class Gamepad extends Joystick {
+
+	public Gamepad(int port, int numAxisTypes, int numButtonTypes) {
+		super(port, numAxisTypes, numButtonTypes);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/org/usfirst/frc322/FRCTeam0322JavaCBR2015/ThrustmasterJoystick.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc322.FRCTeam0322JavaCBR2015;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class ThrustmasterJoystick extends Joystick {
+
+	public ThrustmasterJoystick(int port, int numAxisTypes, int numButtonTypes) {
+		super(port, numAxisTypes, numButtonTypes);
+		// TODO Auto-generated constructor stub
+	}
+
+}
