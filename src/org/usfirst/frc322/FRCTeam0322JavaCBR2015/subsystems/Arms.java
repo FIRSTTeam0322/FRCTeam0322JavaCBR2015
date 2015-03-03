c96cc6d19ee921791dee81c837b6dc54fc565a16
Raa'Shaun H. <CardcaptorRaaShaun@gmail.com>
2015-03-03 18:02:12 -0500

    ArmActuator Setup
    I needed to set a default Command for the arms to make them work. (

--- src/org/usfirst/frc322/FRCTeam0322JavaCBR2015/subsystems/Arms.java
+++ src/org/usfirst/frc322/FRCTeam0322JavaCBR2015/subsystems/Arms.java
@@ -41,6 +41,7 @@ public class Arms extends Subsystem {
 	
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    	setDefaultCommand(new ArmActuator());
     }
     
     public void leftArmControl(double speed) {
