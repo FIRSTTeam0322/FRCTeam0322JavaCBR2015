9a2d7844a1ff31550439d0fe600661597cf13ca6
Raa'Shaun Hunter <RaaShaun.Hunter@Gmail.com>
2015-03-07 00:01:58 -0500

    Finished Basic Auton Mode

--- src/org/usfirst/frc322/FRCTeam0322JavaCBR2015/commands/AutonMoveForward.java
+++ src/org/usfirst/frc322/FRCTeam0322JavaCBR2015/commands/AutonMoveForward.java
@@ -20,20 +20,26 @@ public class AutonMoveForward extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	
+    	Robot.chassis.autoDriveSystem(0.75, 0.0, 0.0);
+    		
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+    	if (this.isTimedOut())
+    		return true;
+    	else
+    		return false;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.chassis.autoDriveSystem(0.0, 0.0, 0.0);
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
