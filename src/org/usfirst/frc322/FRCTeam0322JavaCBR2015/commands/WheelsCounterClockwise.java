719189ebeecd07006ff88fa1b131955a4ef959b1
Raa'Shaun H. <CardcaptorRaaShaun@gmail.com>
2016-01-25 14:16:26 -0500

    Rename WheelsCounterclockwise.java to WheelsCounterClockwise.java

---
+++ src/org/usfirst/frc322/FRCTeam0322JavaCBR2015/commands/WheelsCounterClockwise.java
@@ -0,0 +1,45 @@
+package org.usfirst.frc322.FRCTeam0322JavaCBR2015.commands;
+
+import org.usfirst.frc322.FRCTeam0322JavaCBR2015.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class WheelsCounterClockwise extends Command {
+
+    public WheelsCounterClockwise() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.leftArmWheel);
+    	requires(Robot.rightArmWheel);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.leftArmWheel.leftWheelControl(1.0);
+    	Robot.rightArmWheel.rightWheelControl(1.0);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.leftArmWheel.leftWheelControl(0.0);
+    	Robot.rightArmWheel.rightWheelControl(0.0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
